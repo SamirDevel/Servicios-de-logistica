6d8237186fafa560c644f7eb58538f8671c0cf31
Samir Priego <samir.apriego@gmail.com>
2024-06-17 16:00:52 -0600

    1.3.0
    Preparacion de la seccion de choferes

---
+++ src/Common/Utilities/Tools/Builders/Chofer.Builder.ts
@@ -0,0 +1,6 @@
+import MainQueryBuilder from "../Classes/MainQueryBuilder";
+import Chofer from "../../../../Common/Entities/Chofer.entity";
+
+export default class ChoferBuilder extends MainQueryBuilder<Chofer>{
+    
+}
